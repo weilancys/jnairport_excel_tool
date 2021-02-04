d8369f277ac4e9843650ce1b2c63faa539186277
lightblue <weilancys@hotmail.com>
2021-02-04 15:54:02 +0800

    updated version number to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name = 'JNAirport_Excel_Tool',
-    version = "0.0.1",
+    version = "0.0.2",
     author="lbcoder",
     author_email="lbcoder@hotmail.com",
     description="A excel helper tool for jinan airport",
